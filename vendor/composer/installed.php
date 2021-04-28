4c61556d311d4399a595cdb299df8046d85ef343
X-Ryl669 <boite.pour.spam@gmail.com>
2021-04-28 16:16:49 +0200

    Add vendor code

---
+++ vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => 'd5dbb134544d9cd19e28ce51b0e31655bf26d315',
+    'name' => 'x-ryl669/viewer3-d',
+  ),
+  'versions' => 
+  array (
+    'x-ryl669/viewer3-d' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'd5dbb134544d9cd19e28ce51b0e31655bf26d315',
+    ),
+  ),
+);
