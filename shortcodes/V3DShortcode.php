7c913030a8eac322600fe2df262767ceffe02b71
X-Ryl669 <boite.pour.spam@gmail.com>
2021-07-29 19:05:16 +0200

    Fix missing config when first loaded

--- shortcodes/V3DShortcode.php
+++ shortcodes/V3DShortcode.php
@@ -15,12 +15,14 @@ class V3DShortcode extends Shortcode
             $this->shortcode->addAssets('js', 'plugin://viewer3d/assets/js/3dviewer.js');
             $slug = $this->grav['page']->slug();
             $baseUrl = Utils::url('plugin://viewer3d/assets/img');
-            $showEdges = $this->grav['config']->get('plugins.viewer3d')['show_edges'];
-            $showCube = $this->grav['config']->get('plugins.viewer3d')['show_cube'];
-            $showCut = $this->grav['config']->get('plugins.viewer3d')['show_cut'];
-            $showMeasure = $this->grav['config']->get('plugins.viewer3d')['show_measure'];
-            $showInfopanel = $this->grav['config']->get('plugins.viewer3d')['show_infopanel'];
-            $showDownload = $this->grav['config']->get('plugins.viewer3d')['show_download'];
+            $config = $this->grav['config']->get('plugins.viewer3d');
+
+            $showEdges     = $config['show_edges'] ?? false;
+            $showCube      = $config['show_cube'] ?? true;
+            $showCut       = $config['show_cut'] ?? false;
+            $showMeasure   = $config['show_measure'] ?? false;
+            $showInfopanel = $config['show_infopanel'] ?? true;
+            $showDownload  = $config['show_download'] ?? true;
 
             $modelPath = $slug.'/'.$sc->getContent();
             $out = "<div class='online_3d_viewer' data-base='".$baseUrl."/' model='".$modelPath."' camera='-1.5,-3.0,2.0,0,0,0,0,0,1'".($showEdges ? " edge='#000000'" : "")." color='#A0B0A0'".($showCube ? " navcube='1'": "").">";
